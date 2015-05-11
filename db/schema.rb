8edff9541cfec46c941237a585d52c9bac5beeaf
Ilya Faybisovich <Ilya.Faybisovich@gmail.com>
2015-05-11 19:05:42 +0300

    Title of Amazon item can be seen now

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150511115333) do
+ActiveRecord::Schema.define(version: 20150511154653) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 20150511115333) do
     t.float    "budget"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text     "title"
   end
 
 end
