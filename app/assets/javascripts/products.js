1eadea3ff71a7608288dcd69901f0f2debe9d549
Dan Blakeman <DanBlakeman@users.noreply.github.com>
2015-05-12 11:27:17 +0100

    rows which are affordable go green

--- app/assets/javascripts/products.js
+++ app/assets/javascripts/products.js
@@ -1,19 +1,28 @@
 $(document).on('ready page:load',function() {
 
   $('.product_new_form').hide()
-
   $('.header_button').click(function(e) {
     e.preventDefault();
     $(this).fadeOut(100, function() {
       $('.product_new_form').fadeIn(200);
     });
-
     $('.watch_button').click(function(e) {
       if ($('.project_url').val() === "" || $('.project_budget').val() === "" ) {
         e.preventDefault();
       };
     });
+  });
+
+  $('.table tr').each(function() {
+
+    var current_price = $(this).find('.current_price').data('number');
+    var budget = $(this).find('.budget').data('number');
+
+    if (current_price <= budget ) {
+      $(this).css('background-color', '#98FB98')
+    }
 
   });
+
 
 });
